5e3048dc331bd2eab6e4548bbb45dc63f1863b6a
Tiagalex <tiagoavrodrigues4@gmail.com>
2025-01-24 11:48:37 +0000

    just a start

---
+++ push_swap.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiagalex <tiagalex@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 16:32:46 by tiagalex          #+#    #+#             */
+/*   Updated: 2025/01/23 17:37:18 by tiagalex         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stddef.h>
+
+//Funtions
+
+typedef struct node
+{
+	int pre;
+	int	content;
+	int	next;
+	
+}	node;
+
+
+#endif
