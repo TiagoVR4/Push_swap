310c20e5aa9dcb330b0b19753d655ba312fa5633
Tiagalex <tiagoavrodrigues4@gmail.com>
2025-01-28 19:39:21 +0000

    a

---
+++ main.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiagalex <tiagalex@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 16:50:37 by tiagalex          #+#    #+#             */
+/*   Updated: 2025/01/23 16:50:37 by tiagalex         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int main(int argc, char **argv)
+{
+	node	*stack_a;
+
+	stack_a = NULL;
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
+		return (1);
+	if (ft_error(?) || argv[1] == NULL)
+	{
+		//freestack(stack_a)
+		ft_printf("Error");
+		return (0);
+	}
+	if (/* sorted */)
+	{
+		//freestack(stack_a)
+		return (0);
+	}
+	//push_swap(agrc, stack_a)
+	//free_stack(stack_a)
+	return (0);
+}
