7fb26cc4cd1dfbcce5426709c881258f774f0874
Tiagalex <tiagoavrodrigues4@gmail.com>
2025-02-25 18:02:59 +0000

    start ft_parsing still alot to do

--- ft_parsing.c
+++ ft_parsing.c
@@ -6,15 +6,34 @@
 /*   By: tiagalex <tiagalex@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 11:42:06 by tiagalex          #+#    #+#             */
-/*   Updated: 2025/02/25 11:57:06 by tiagalex         ###   ########.fr       */
+/*   Updated: 2025/02/25 18:02:04 by tiagalex         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_parsing(t_node **stack_a, int argc, char **argv)
+int	ft_parsing(t_node **stack_a, int argc, char **argv)
 {
+	int		i;
+
+	i = 0;
 	if (argc < 2)
-		return ;
-	
+		return (0);
+	if (argc == 2 && need_split(argv[1] == 0))
+	{
+		char	*clone;
+
+		clone = argv[1];
+		stack_a = ft_split(argv[1], " ");
+		while (stack_a[i] <= argc)
+		{
+			if (is_number())
+			{
+				write(2, "not a number", 12);
+				free_stack(stack_a);
+				return (0);
+			}
+			 = ft_atoi(stack_a[i]);
+		}
+	}
 }
