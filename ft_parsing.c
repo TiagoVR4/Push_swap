690b32cbf7853b04b882f4c038cd02f81ccfeb40
TiagoVR4 <tiagoavrodrigues4@gmail.com>
2025-02-27 12:34:16 +0000

    maybe finish the ft_parsing need to test

--- ft_parsing.c
+++ ft_parsing.c
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 11:42:06 by tiagalex          #+#    #+#             */
-/*   Updated: 2025/02/26 18:38:23 by coder            ###   ########.fr       */
+/*   Updated: 2025/02/27 12:32:16 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,36 +17,71 @@ Ola Tiago do futuro eh a Jessica, alias va a merda! voce pode fazercom que as fu
 uma unica linha e assim pode te "salvar" linhas e cabelos brancos ate ja _|_ se precisar de ajuda me avisa
 int	ft_parsing(t_node **stack_a, int argc, char **argv)
 {
-	int		i;
-
+	int	i;
+	
 	i = 0;
 	if (argc < 2)
 		return (0);
-	if (argc == 2 && need_split(argv[1] == 0))
+	else if (argc == 2 && contains_space(argv[1]) == 0)
 	{
-		char	*clone;
 		char	**str;
 
-		clone = argv[1];
-		**str = ft_split(clone, " ");
-		while (i < argc)
+		str = ft_split(argv[1], " ");
+		while (str[i] != '\0')
 		{
 			if (is_not_number(str[i]) || is_not_int(str[i]))
 			{
 				write(2, "not a int", 9);
 				free_stack(stack_a);
-				exit;
+				return (1);
 			}
 			if (duplicate(str))
 			{
 				write(2, "is Duplicated", 13);
 				free_stack(stack_a);
-				exit (0);
+				return (1);
 			}
+			add_node(stack_a, str);
+			i++;
+			free(str[i]);
+		}
+	}
+	else
+	{
+		i = 1;
+		while (i > argc)
+		{
+			if (is_not_number(argv[i]) || is_not_int(argv[i]))
+			{
+				write(2, "not a int", 9);
+				free_stack(stack_a);
+				return (1);
+			}
+			if (duplicate(argv[i]))
+			{
+				write(2, "is Duplicated", 13);
+				free_stack(stack_a);
+				return (1);
+			}
+			add_node(stack_a, argv[i]);
 			i++;
 		}
 	}
-	if ()
+	return (0);
+}
+
+int	contains_space(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i] != '\0')
+	{
+		if (str[i] == ' ')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 int	is_not_number(char	*str)
