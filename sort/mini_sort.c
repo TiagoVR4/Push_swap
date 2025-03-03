e85f2efc75aa67c1c5852deea3a22d1e68514b8a
Tiagalex <tiagoavrodrigues4@gmail.com>
2025-03-03 16:52:10 +0000

    finish mini_sort

--- sort/mini_sort.c
+++ sort/mini_sort.c
@@ -6,7 +6,7 @@
 /*   By: tiagalex <tiagalex@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 18:22:00 by tiagalex          #+#    #+#             */
-/*   Updated: 2025/03/03 12:40:34 by tiagalex         ###   ########.fr       */
+/*   Updated: 2025/03/03 16:44:45 by tiagalex         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,26 @@ void	mini_sort(t_node **stack)
 	t_node	*first;
 
 	first = *stack;
-	if (first->value < first->next->value && )
+	if (first->value < first->next->value)
+	{
+		if (first->value > first->prev->value)
+			call_rrotate(stack, NULL, 'a');
+		else
+		{
+			call_swap(stack, NULL, 'a');
+			call_rotate(stack, NULL, 'a');
+		}
+	}
+	else
+	{
+		if (first->value < first->prev->value)
+			call_swap(stack, NULL, 'a');
+		else if (first->next->value > first->prev->value)
+		{
+			call_swap(stack, NULL, 'a');
+			call_rrotate(stack, NULL, 'a');
+		}
+		else
+			call_rotate(stack, NULL, 'a');
+	}
 }
