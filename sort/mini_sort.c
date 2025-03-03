ecf87ce8651c99252611b0d713f43025ed7ffe64
Tiagalex <tiagoavrodrigues4@gmail.com>
2025-03-03 12:41:10 +0000

    nothing done

--- sort/mini_sort.c
+++ sort/mini_sort.c
@@ -6,7 +6,7 @@
 /*   By: tiagalex <tiagalex@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 18:22:00 by tiagalex          #+#    #+#             */
-/*   Updated: 2025/03/02 18:26:48 by tiagalex         ###   ########.fr       */
+/*   Updated: 2025/03/03 12:40:34 by tiagalex         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 
 void	mini_sort(t_node **stack)
 {
-	
+	t_node	*first;
+
+	first = *stack;
+	if (first->value < first->next->value && )
 }
