d8f220c75a3256c465a4f52ba9c0773e1a11a93d
Tiagalex <tiagoavrodrigues4@gmail.com>
2025-03-06 14:21:08 +0000

    create function medium_sort need to test it

--- sort/medium_sort.c
+++ sort/medium_sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   medium_sort.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*   By: tiagalex <tiagalex@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 16:57:14 by tiagalex          #+#    #+#             */
-/*   Updated: 2025/03/05 16:03:50 by coder            ###   ########.fr       */
+/*   Updated: 2025/03/06 14:18:06 by tiagalex         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,48 @@
 
 void	medium_sort(t_node **stack_a, t_node **stack_b)
 {
-		t_node	*first;
-		t_node	*second;
-		t_node	*third;
-		t_node	*fourth;
-
-		t_node = 
-		if (size_stack(stack_a) == 5)
+	t_node	*current;
+	int		i;
+	int		pos;
+	
+	current = *stack_a;
+	i = 0;
+	pos = 0;
+	while (stack_size(stack_a) > 3)
+	{
+		if (current->index == i)
 		{
-			t_node	*fifth;
-			
-			fifth = first->prev;
-			fourth = fifth->prev;
+			index_top(stack_a, stack_b, pos);
+			call_push(stack_a, stack_b, 'b');
+			i++;
+			pos = -1;
+			current = (*stack_a)->prev;
 		}
-		else
-			fourth = first->prev;
-		
+		current = current->next;
+		pos++;
+	}
+	mini_sort(stack_a);
+	if(stack_size(*stack_b) == 2)
+		call_push(stack_a, stack_b, 'a');
+	call_push(stack_a, stack_b, 'a');
+}
+void	index_top(t_node **stack_a, t_node **stack_b, int pos)
+{
+	if (pos <= stack_size(*stack_a) / 2)
+	{
+		while (pos > 0)
+		{
+			call_rotate(*stack_a, stack_b, 'a');
+			pos--;
+		}
+	}
+	else
+	{
+		pos = stack_size(*stack_a) - pos;
+		while (pos > 0)
+		{
+			call_rrotate(stack_a, stack_b, 'a');
+			pos--;
+		}
+	}
 }
