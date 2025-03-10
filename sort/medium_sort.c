3bdff2bc09d89ad7c32f5dac86d1974a2c989bed
TiagoVR4 <tiagoavrodrigues4@gmail.com>
2025-03-10 16:40:28 +0000

    add assign_chunks

--- sort/medium_sort.c
+++ sort/medium_sort.c
@@ -59,3 +59,27 @@ void	index_top(t_node **stack_a, t_node **stack_b, int pos)
 		}
 	}
 }
+
+void	assign_chunk(t_node **stack)
+{
+	int		n;
+	int		num_chunks;
+	int		chunk_size;
+	t_node	*current;
+
+	n = stack_size(stack);
+	num_chunks = ((n + 49) / 50);
+	if (num_chunks < 2)
+		num_chunks = 2;
+	chunk_size = n / num_chunks;
+	current = *stack;
+	while (current)
+	{
+		current->chunk = current->index / chunk_size;
+		if(current->chunk >= num_chunks)
+			current->chunk = num_chunks - 1;
+		current = current->next;
+		if (current == *stack)
+			break;
+	}
+}
