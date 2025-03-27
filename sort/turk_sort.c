2229d419bbe75549106fe5ffa1979e4fe8ff6c4c
Tiagalex <tiagoavrodrigues4@gmail.com>
2025-03-27 16:06:04 +0000

    Revert "fix sort_b, still need optimize sorting_a"
    This reverts commit b423f467f4744fe46a07a6bd3d55440caadc03a1.

--- sort/turk_sort.c
+++ sort/turk_sort.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   turk_sort.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tiagvr <tiagvr@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tiagalex <tiagalex@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 16:23:57 by tiagalex          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/03/27 11:06:48 by tiagvr           ###   ########.fr       */
-=======
-/*   Updated: 2025/03/26 18:58:05 by tiagalex         ###   ########.fr       */
->>>>>>> parent of b423f46 (fix sort_b, still need optimize sorting_a)
+/*   Updated: 2025/03/27 16:05:26 by tiagalex         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,11 +32,7 @@ void	turk_sort(t_node **stack_a, t_node **stack_b)
 		if (current->chunk == current_chunk)
 		{
 			target = current->index;
-<<<<<<< HEAD
-			ft_printf("Sorting: pos = %d, index target = %d\n", pos, target); // log
-=======
-			// ft_printf("Sorting: pos=%d, target=%d\n", pos, target); // log
->>>>>>> parent of b423f46 (fix sort_b, still need optimize sorting_a)
+			// ft_printf("Sorting: pos = %d, index target = %d\n", pos, target); // log
 			sort_b(stack_a, stack_b, pos, target);
 			pos = 0;
 			current = *stack_a;
@@ -51,11 +43,7 @@ void	turk_sort(t_node **stack_a, t_node **stack_b)
 			current = current->next;
 		}
 		update_chunk(stack_a, &current, &current_chunk, &pos);
-<<<<<<< HEAD
-	  	printf("Updated chunk: %d, pos: %d\n", current_chunk, pos); // log
-=======
-	  //  printf("Updated chunk: %d, pos: %d\n", current_chunk, pos); // log
->>>>>>> parent of b423f46 (fix sort_b, still need optimize sorting_a)
+	  	// printf("Updated chunk: %d, pos: %d\n", current_chunk, pos); // log
 	}
 }
 
